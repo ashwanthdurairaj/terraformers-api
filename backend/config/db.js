4c6410bb323877531f7fd4c28caa7ded50e95c88
ashwanthdurairaj <69207745+ashwanthdurairaj@users.noreply.github.com>
2023-04-30 23:19:49 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI)
+    const conn = await mongoose.connect('mongodb+srv://ashwanth:ash@psa.7xyevjn.mongodb.net/terraformers?retryWrites=True&w=majority')
 
     console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline)
   } catch (error) {
