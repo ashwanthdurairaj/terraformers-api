fd95357aac8e1d9cbd4063d5d84e25793376781c
ashwanthdurairaj <69207745+ashwanthdurairaj@users.noreply.github.com>
2023-05-01 00:49:05 +0530

    Update authMiddleware.js

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -13,7 +13,7 @@ const protect = asyncHandler(async (req, res, next) => {
     try {
       token = req.headers.authorization.split(' ')[1]
       console.log(token)
-      const decoded = jwt.verify(token, process.env.JWT_SECRET)
+      const decoded = jwt.verify(token, 'fae32013r')
 
       req.user = await User.findById(decoded.id).select('-password')
       console.log("hi",req.user)
