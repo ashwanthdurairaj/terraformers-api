34518ced88209a187870ea8eca77ab45dcea28d6
ashwanthdurairaj <69207745+ashwanthdurairaj@users.noreply.github.com>
2023-04-30 23:08:54 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const colors = require('colors');
+const dotenv = require('dotenv');
+dotenv.config();
+const { errorHandler } = require('./middleware/errorMiddleware');
+const connectDB = require('./config/db');
+const port = process.env.PORT || 3000;
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  next();
+});
+app.use('/users', require('./routes/userRoutes'))
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/build')));
+  
+    app.get('*', (req, res) =>
+      res.sendFile(
+        path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+      )
+    );
+  } else {
+    app.get('/', (req, res) => res.send('Please set to production'));
+  }
+  
+app.use(errorHandler);
+  
+app.listen(port, () => console.log(`Server started on port ${port}`));
+  
