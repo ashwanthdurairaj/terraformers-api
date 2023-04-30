5dab46ed8a58813d8aba6e3c7d0103f7f51b099f
ashwanthdurairaj <ashwanthdurairaj2003@gmail.com>
2023-05-01 02:44:33 +0530

    update user model

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -9,7 +9,7 @@ const userSchema = mongoose.Schema(
     email: {
       type: String,
       required: [true, 'Please add an email'],
-      unique: true,
+      // unique: true,
     },
     password: {
       type: String,
